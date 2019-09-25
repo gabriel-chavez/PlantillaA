076693ed4979147dc3645531914e37374646d36d
gabriel <gabriel.chavez.r@gmail.com>
2019-09-25 07:19:30 -0400

    Inicio

---
+++ src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">UNIVida todos los derechos reservados 2019</span>
+
+  `,
+})
+export class FooterComponent {
+}
